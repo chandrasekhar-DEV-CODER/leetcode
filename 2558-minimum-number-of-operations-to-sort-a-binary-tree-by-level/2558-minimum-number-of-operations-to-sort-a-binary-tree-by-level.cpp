edfc6db3968b841521482c2d5e7839b809e1b550
chandrasekhar-DEV-CODER <chandrasekharsathi.dev@gmail.com>
2024-12-23 18:18:19 +0530

    Time: 176 ms (34.31%), Space: 217.2 MB (15.83%) - LeetHub

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -1,32 +1,54 @@
 class Solution {
- public:
-  int minimumOperations(TreeNode* root) {
-    int ans = 0;
-    queue<TreeNode*> q{{root}};
+public:
+    int minimumOperations(TreeNode* root) 
+    {
+        if(root == nullptr)
+            return 0;
+        //BFS + for each level find sort for min swap
+        //values array, sorted values array
+        //both should match -if not need swap and count the #of swaps
+        //but to find correct Index, we can use map( unordered) with value as key and index as valuue.
+        queue<TreeNode*> q;       
+        int totalswaps  = 0;
+        q.push(root);
 
-    // e.g. vals = [7, 6, 8, 5]
-    // [2, 1, 3, 0]: Initialize the ids based on the order of vals.
-    // [3, 1, 2, 0]: Swap 2 with 3, so 2 is in the right place (i == ids[i]).
-    // [0, 1, 2, 3]: Swap 3 with 0, so 3 is in the right place.
-    while (!q.empty()) {
-      vector<int> vals;
-      vector<int> ids(q.size());
-      for (int sz = q.size(); sz > 0; --sz) {
-        TreeNode* node = q.front();
-        q.pop();
-        vals.push_back(node->val);
-        if (node->left != nullptr)
-          q.push(node->left);
-        if (node->right != nullptr)
-          q.push(node->right);
-      }
-      iota(ids.begin(), ids.end(), 0);
-      ranges::sort(ids, [&vals](int i, int j) { return vals[i] < vals[j]; });
-      for (int i = 0; i < ids.size(); ++i)
-        for (; ids[i] != i; ++ans)
-          swap(ids[i], ids[ids[i]]);
+        while(!q.empty())
+        {
+            int len = q.size();
+            vector<int> values;
+            unordered_map<int, int> valuemap;
+            for(int i=0; i<len; i++)
+            {
+                TreeNode* temp = q.front();
+                q.pop();
+                values.push_back(temp->val);
+                valuemap[temp->val] = i;
+
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
+            }
+         
+            vector<int> sortedvalues = values;
+            sort(sortedvalues.begin(), sortedvalues.end());
+
+            for(int i=0; i<values.size(); i++)
+            {
+                if(sortedvalues[i] != values[i])
+                {
+                    totalswaps++;
+                    int correctIndex = valuemap[sortedvalues[i]];
+                    swap(values[i], values[correctIndex]);
+                    valuemap[values[correctIndex]] = correctIndex;
+                    valuemap[values[i]] = i;                   
+                }
+            }
+        }
+        return totalswaps;  
     }
-
-    return ans;
-  }
 };
