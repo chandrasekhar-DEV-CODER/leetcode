1852247366e0a33b932e8689eaa3032d65b15913
chandrasekhar-DEV-CODER <chandrasekharsathi.dev@gmail.com>
2025-01-30 19:55:10 +0530

    Time: 0 ms (100%), Space: 9.2 MB (97.44%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& mat) {
-        vector<int> ans;
- 
-  int n = mat.size(); // no. of nows
-  int m = mat[0].size(); // no. of columns
-  
-  // Initialize the pointers reqd for traversal.
-  int top = 0, left = 0, bottom = n - 1, right = m - 1;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) { 
+        if (matrix.empty()) return {};
 
-  // Loop until all elements are not traversed.
-  while (top <= bottom && left <= right) {
-      
-    // For moving left to right
-    for (int i = left; i <= right; i++)
-      ans.push_back(mat[top][i]);
-
-    top++;
-
-    // For moving top to bottom.
-    for (int i = top; i <= bottom; i++)
-      ans.push_back(mat[i][right]);
-
-    right--;
-    
-    // For moving right to left.
-    if (top <= bottom) {
-      for (int i = right; i >= left; i--)
-       ans.push_back(mat[bottom][i]);
-
-      bottom--;
-    }
-
-    // For moving bottom to top.
-    if (left <= right) {
-      for (int i = bottom; i >= top; i--)
-        ans.push_back(mat[i][left]);
-
-      left++;
-    }
-  }
-  return ans;
+        int t = 0, l = 0;
+        int b = matrix.size() - 1;  // Fix: Number of rows - 1
+        int r = matrix[0].size() - 1;
+        vector<int>a;
+        while(t<=b && l<=r){
+            for(int i=l;i<=r;i++)
+            {
+                a.push_back(matrix[t][i]);
+            }
+            t++;
+            for(int i=t;i<=b;i++)
+            {
+                a.push_back(matrix[i][r]);
+            }
+            r--;
+            if(t<=b){
+                for(int i=r;i>=l;i--){
+                    a.push_back(matrix[b][i]);
+                }
+                b--;
+            }
+            if(l<=r){
+                for(int i=b;i>=t;i--){
+                    a.push_back(matrix[i][l]);
+                }
+                l++;
+            }
+        }
+        return a;
     }
 };
