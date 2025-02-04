ff2d3dc8f8ba2c68e37ffdf5688ecb49e2b2501f
chandrasekhar-DEV-CODER <chandrasekharsathi.dev@gmail.com>
2025-02-04 20:01:31 +0530

    Time: 114 ms (36.65%), Space: 99.8 MB (30.29%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-    unordered_set<int> windowSet;
-    long long maxSum = 0, currentSum = 0;
-    int n = nums.size();
-    int left = 0;
-    for (int right = 0; right < n; ++right) {
-        while (windowSet.count(nums[right])) {
-            currentSum -= nums[left];
-            windowSet.erase(nums[left]);
-            ++left;
+        long long l=0,r=0,m=0,su=0;
+        unordered_set<long long>s;
+        while(r<nums.size()){
+            while(s.find(nums[r])!=s.end()){
+                s.erase(nums[l]);
+                su-=nums[l];
+                l++;
+            }
+            su+=nums[r];
+            s.insert(nums[r]);
+            if(r-l+1>k){
+                s.erase(nums[l]);
+                su-=nums[l];
+                l++;
+            }
+            if(r-l+1==k){
+                m=max(m,su);
+                
+            }
+            r++;
         }
-        windowSet.insert(nums[right]);
-        currentSum += nums[right];
-        if (right - left + 1 == k) {
-            maxSum = max(maxSum, currentSum);
-            currentSum -= nums[left];
-            windowSet.erase(nums[left]);
-            ++left;
-        }
-    }
-    return maxSum;
+        return m;
     }
 };
