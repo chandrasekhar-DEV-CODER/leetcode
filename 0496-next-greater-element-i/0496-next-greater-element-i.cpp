7291643e2259c2aba103397f87ff914b5a5cf650
SATHI CHANDRA SEKHAR MANIKANTA REDDY <chandrasekharsathi.dev@gmail.com>
2026-03-27 16:00:04 +0530

    Time: 7 ms (10.83%), Space: 12.5 MB (81.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-      vector<int>s(nums1.size(), -1);
-      int c=0;
-        for(int i=0;i<nums1.size();i++)
-        {int j=0;
-            while (j < nums2.size() && nums2[j] != nums1[i]) {
-                j++;
+        stack<int> a;
+        vector<int> s(nums2.size());
+        vector<int> r(nums1.size());
+        
+        for(int i = nums2.size() - 1; i >= 0; i--) {
+            while(!a.empty() && a.top() <= nums2[i]) {
+                a.pop();
             }
-                if (j < nums2.size()) {
-                for (int k = j + 1; k < nums2.size(); k++) {
-                    if (nums2[k] > nums1[i]) {
-                        s[c] = nums2[k]; 
-                        break;
-                    }
+            if(a.empty()) s[i] = -1;
+            else s[i] = a.top();
+            a.push(nums2[i]); 
+        }
+        
+        for(int i = 0; i < nums1.size(); i++) {
+            for(int j = 0; j < nums2.size(); j++) {
+                if(nums1[i] == nums2[j]) {     
+                    r[i] = s[j];               
+                    break;                      
                 }
             }
-                c++;
-            }
-        return s;
+        }
+        return r;
     }
 };
