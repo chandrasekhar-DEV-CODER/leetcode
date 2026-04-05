a484c32ffecc44c33e3adff48e57d1280fc8a707
SATHI CHANDRA SEKHAR MANIKANTA REDDY <chandrasekharsathi.dev@gmail.com>
2026-04-05 21:03:37 +0530

    Time: 2 ms (10.04%), Space: 30.5 MB (18.72%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        k%=nums.size();
-        for(int i=0;i<nums.size()/2;i++){
-            swap(nums[i],nums[nums.size()-i-1]);
+        vector<int>a(nums.size());
+        for(int i=0;i<nums.size();i++){
+            a[(i+k)%nums.size()]=nums[i];
         }
-        for(int i=0;i<k/2;i++){
-            swap(nums[i],nums[k-i-1]);
-        }
-        for(int i=0;i<(nums.size()-k)/2;i++){
-            swap(nums[k+i],nums[nums.size()-i-1]);
-        }
+        nums=a;
         
     }
 };
